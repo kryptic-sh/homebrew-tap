31c4e697e67e6f3b7ae0a69761d5d72d4afcf021
mxaddict <mxaddict@kryptic.sh>
2026-05-05 06:08:08 +0800

    buffr 0.4.0

---
+++ Casks/buffr.rb
@@ -0,0 +1,19 @@
+cask "buffr" do
+  version "0.4.0"
+  sha256 "76367d4adf6f5de8589f476642b5f650f3357a3feab3f0de773489dc9dad0311"
+
+  url "https://github.com/kryptic-sh/buffr/releases/download/v#{version}/buffr-#{version}-arm64.dmg"
+  name "buffr"
+  desc "Vim-flavored web browser built on CEF"
+  homepage "https://kryptic.sh"
+
+  depends_on macos: ">= :ventura", arch: :arm64
+
+  app "buffr.app"
+
+  zap trash: [
+    "~/.cache/buffr",
+    "~/.config/buffr",
+    "~/.local/share/buffr",
+  ]
+end
