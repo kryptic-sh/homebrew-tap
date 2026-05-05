5668f7ded206612a1a828ea6c7408c3063d5554e
buffr-ci <ci@kryptic.sh>
2026-05-05 15:44:57 +0000

    buffr 0.5.0

--- Casks/buffr.rb
+++ Casks/buffr.rb
@@ -1,6 +1,6 @@
 cask "buffr" do
-  version "0.4.0"
-  sha256 "76367d4adf6f5de8589f476642b5f650f3357a3feab3f0de773489dc9dad0311"
+  version "0.5.0"
+  sha256 "cd76d7f66562753d62ff30634cea8250e1f9268b5dd49fa988348f71e35ee511"
 
   url "https://github.com/kryptic-sh/buffr/releases/download/v#{version}/buffr-#{version}-arm64.dmg"
   name "buffr"
@@ -9,7 +9,7 @@ cask "buffr" do
 
   depends_on macos: ">= :ventura", arch: :arm64
 
-  app "buffr.app"
+  app "Buffr.app"
 
   zap trash: [
     "~/.cache/buffr",
