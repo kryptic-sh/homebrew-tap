92927ce6245e6e0b868d8edd91e52babbd361849
hodl-ci <ci@kryptic.sh>
2026-05-05 19:50:38 +0000

    hodl 0.7.0

--- Formula/hodl.rb
+++ Formula/hodl.rb
@@ -5,17 +5,17 @@
 class Hodl < Formula
   desc "Light crypto wallet for the terminal — multi-chain TUI built on ratatui"
   homepage "https://hodl.kryptic.sh/"
-  version "0.6.0"
+  version "0.7.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/kryptic-sh/hodl/releases/download/v0.6.0/hodl-v0.6.0-aarch64-apple-darwin.tar.gz"
-      sha256 "9ac022bb6d9366d3b88082a5aced6337c9be44691286811c5707b464fdc74fd4"
+      url "https://github.com/kryptic-sh/hodl/releases/download/v0.7.0/hodl-v0.7.0-aarch64-apple-darwin.tar.gz"
+      sha256 "7fdfb22fb2f90607d9ac62a32117a1fafbbb29ca56b2f7378c7587375a906995"
     end
     on_intel do
-      url "https://github.com/kryptic-sh/hodl/releases/download/v0.6.0/hodl-v0.6.0-x86_64-apple-darwin.tar.gz"
-      sha256 "398dd60f765cd184df859bad011befca44a451af21ea5793f456d55d4d0f9425"
+      url "https://github.com/kryptic-sh/hodl/releases/download/v0.7.0/hodl-v0.7.0-x86_64-apple-darwin.tar.gz"
+      sha256 "fb9f7a37b1138220db3043a1dcb43788bddb406679a5dcb94e432c92b285de91"
     end
   end
 
