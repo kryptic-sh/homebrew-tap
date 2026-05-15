ef9b2d43ccbec1dea4f214988c6ce2ec279ae276
sqeel-ci <ci@kryptic.sh>
2026-05-15 01:29:04 +0000

    sqeel 0.4.18

--- Formula/sqeel.rb
+++ Formula/sqeel.rb
@@ -5,17 +5,17 @@
 class Sqeel < Formula
   desc "Modal SQL workbench for the terminal"
   homepage "https://sqeel.kryptic.sh/"
-  version "0.4.17"
+  version "0.4.18"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/kryptic-sh/sqeel/releases/download/v0.4.17/sqeel-v0.4.17-aarch64-apple-darwin.tar.gz"
-      sha256 "3d4a2f22d9ba137e2aba703dd714836c468f233295c99b129025ba08509c0555"
+      url "https://github.com/kryptic-sh/sqeel/releases/download/v0.4.18/sqeel-v0.4.18-aarch64-apple-darwin.tar.gz"
+      sha256 "44bddc05902c0eed309fb094e46472e5d0138b0cbf2e29115502f682d2386de2"
     end
     on_intel do
-      url "https://github.com/kryptic-sh/sqeel/releases/download/v0.4.17/sqeel-v0.4.17-x86_64-apple-darwin.tar.gz"
-      sha256 "e1e3a523d5eadf825d08c1bd6a6358d6cc854759d52ab4cc0d28d9aed599672c"
+      url "https://github.com/kryptic-sh/sqeel/releases/download/v0.4.18/sqeel-v0.4.18-x86_64-apple-darwin.tar.gz"
+      sha256 "c96a3594d2c2e730e0c47c286dc8c32d489b0d6a9b872794e93e902a3825c426"
     end
   end
 
