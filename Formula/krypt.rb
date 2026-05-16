70d2d9ef0784f7cf6daba004312dbfc77f4b6d05
krypt-ci <ci@kryptic.sh>
2026-05-16 12:07:21 +0000

    krypt 0.1.0

--- Formula/krypt.rb
+++ Formula/krypt.rb
@@ -5,17 +5,17 @@
 class Krypt < Formula
   desc "Cross-platform dotfiles manager. Rust binary. Config-driven."
   homepage "https://github.com/kryptic-sh/krypt"
-  version "0.0.2"
+  version "0.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/kryptic-sh/krypt/releases/download/v0.0.2/krypt-v0.0.2-aarch64-apple-darwin.tar.gz"
-      sha256 "de97a8d0fa9dadaf31733caa228d6fa9735e8d0e8711f67b497f8ee23b911747"
+      url "https://github.com/kryptic-sh/krypt/releases/download/v0.1.0/krypt-v0.1.0-aarch64-apple-darwin.tar.gz"
+      sha256 "051223b94e711b3a358311ec8ff4f8c1946560eb826c4888ed090259c63ee8f2"
     end
     on_intel do
-      url "https://github.com/kryptic-sh/krypt/releases/download/v0.0.2/krypt-v0.0.2-x86_64-apple-darwin.tar.gz"
-      sha256 "9545d1b1a6c285c1928c20ab17c5cddda066d6c0b9ad2e3460a8a44bc605fe54"
+      url "https://github.com/kryptic-sh/krypt/releases/download/v0.1.0/krypt-v0.1.0-x86_64-apple-darwin.tar.gz"
+      sha256 "fe85d97e4fe0d27193522873d61bd9da88a7fcc23426a0314f5698d717ca31a1"
     end
   end
 
