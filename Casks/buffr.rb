bcae41316bd84a8bb2a4fa828a8a08e51df07a05
buffr-ci <ci@kryptic.sh>
2026-05-19 14:45:22 +0000

    buffr 0.13.8

--- Casks/buffr.rb
+++ Casks/buffr.rb
@@ -1,6 +1,6 @@
 cask "buffr" do
-  version "0.13.6"
-  sha256 "3e68177f0c2af52ce835c89ef896d2c5fdf51af6fd657f73529bd3ab274d952d"
+  version "0.13.8"
+  sha256 "7e86706a8e8a0991d1891105d09d9386293089d1b7c0c056ee543448b5b270f4"
 
   url "https://github.com/kryptic-sh/buffr/releases/download/v#{version}/buffr-#{version}-arm64.dmg"
   name "buffr"
