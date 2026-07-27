f50e05eedd4fdebda5cb82640a201265d333d914
gpur-ci <ci@kryptic.sh>
2026-07-27 09:40:40 +0000

    gpur 0.9.0

--- Formula/gpur.rb
+++ Formula/gpur.rb
@@ -3,19 +3,19 @@
 # next tag — change `pkg/homebrew/gpur.rb.in` in the upstream repo and
 # let `.github/workflows/ci.yml` rebuild on the next release.
 class Gpur < Formula
-  desc "btop-style GPU monitor TUI — NVIDIA, AMD, Apple Silicon"
+  desc "btop-style GPU monitor TUI — NVIDIA, AMD, Intel, Apple Silicon"
   homepage "https://github.com/kryptic-sh/gpur"
-  version "0.8.1"
+  version "0.9.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/kryptic-sh/gpur/releases/download/v0.8.1/gpur-v0.8.1-aarch64-apple-darwin.tar.gz"
-      sha256 "6404adb592d1a9780960dc83caafbebf9c1302bcc96a9c771c4f4ca98bfde3e2"
+      url "https://github.com/kryptic-sh/gpur/releases/download/v0.9.0/gpur-v0.9.0-aarch64-apple-darwin.tar.gz"
+      sha256 "5f7160fa0fdc560b605fff1425efccea44e09e3a9675d3c7e65f8f027c0db6d2"
     end
     on_intel do
-      url "https://github.com/kryptic-sh/gpur/releases/download/v0.8.1/gpur-v0.8.1-x86_64-apple-darwin.tar.gz"
-      sha256 "7681cb0d33260fe899ff7ab33206084389903b21887cfe17f9cc1f8ed056b2e5"
+      url "https://github.com/kryptic-sh/gpur/releases/download/v0.9.0/gpur-v0.9.0-x86_64-apple-darwin.tar.gz"
+      sha256 "4059e6fb1ff2f352f99b1fc3abf7f592f9e29d06391846096ca3c4429eaa221f"
     end
   end
 
